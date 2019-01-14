02dd25f7ec141fa48525ba825d43b0a2fb93f278
Drew Johnson <drew.a.johnson@outlook.com>
2019-01-14 07:38:03 -0800

    Add database project

---
+++ StarTrekDb/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT        NOT NULL,
+    [FirstName] NCHAR (30) NOT NULL,
+    [LastName]  NCHAR (30) NULL,
+    [UserId]    NCHAR (20) NOT NULL,
+    [Password]  NCHAR (30) NOT NULL
+);
+
